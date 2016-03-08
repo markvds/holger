94f785fc128706c3126a3cccf8288809f52f29b8
David Bohn <david@cancrisoft.net>
2016-03-08 23:38:53 +0100

    Added test for WANStats

---
+++ tests/WANStatsTest.php
@@ -0,0 +1,51 @@
+<?php
+
+
+class WANStatsTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Holger\WANStats
+     */
+    protected $wanStats;
+
+    protected function setUp()
+    {
+        $conn = new \Holger\TR064Connection($_ENV['ROUTER_HOST'], $_ENV['ROUTER_PASSWORD'], $_ENV['ROUTER_USERNAME']);
+        $this->wanStats = new \Holger\WANStats($conn);
+    }
+
+    /** @test */
+    public function it_can_fetch_link_properties()
+    {
+        $link = $this->wanStats->linkProperties();
+
+        $this->assertArrayHasKey('NewWANAccessType', $link);
+        $this->assertArrayHasKey('NewLayer1UpstreamMaxBitRate', $link);
+        $this->assertArrayHasKey('NewLayer1DownstreamMaxBitRate', $link);
+        $this->assertArrayHasKey('NewPhysicalLinkStatus', $link);
+
+    }
+
+    /** @test */
+    public function it_can_fetch_transferred_bytes()
+    {
+        $stats = $this->wanStats->byteStats();
+
+        $this->assertArrayHasKey('sent', $stats);
+        $this->assertTrue(is_numeric($stats['sent']));
+        $this->assertArrayHasKey('received', $stats);
+        $this->assertTrue(is_numeric($stats['received']));
+
+    }
+
+    /** @test */
+    public function it_can_fetch_transferred_packets()
+    {
+        $stats = $this->wanStats->packetStats();
+
+        $this->assertArrayHasKey('sent', $stats);
+        $this->assertTrue(is_numeric($stats['sent']));
+        $this->assertArrayHasKey('received', $stats);
+        $this->assertTrue(is_numeric($stats['received']));
+    }
+}
