952bd3d972ab0db9d27795b5edc5f4776ca79b4d
David Bohn <david@cancrisoft.net>
2016-03-09 22:36:26 +0100

    Added Network lib

--- src/Network.php
+++ src/Network.php
@@ -14,17 +14,34 @@ class Network
 
     use HasEndpoint;
 
+    /**
+     * Fetches the number of known hosts
+     * These are the members of the network that have been registered some time.
+     * @return int
+     */
     public function numberOfHostEntries()
     {
         return (int)$this->prepareRequest()->GetHostNumberOfEntries();
     }
 
+    /**
+     * Get information about one peer in the local network.
+     * Data includes the IP address (NewIPAddress), MAC Address (NewMACAddress)
+     * and much more.
+     * @param $id
+     * @return array
+     */
     public function hostById($id)
     {
         $idParam = new \SoapParam($id, "NewIndex");
         return $this->prepareRequest()->GetGenericHostEntry($idParam);
     }
 
+    /**
+     * Get information like IP address of a host given by the mac address
+     * @param $mac
+     * @return mixed
+     */
     public function hostByMAC($mac)
     {
         $macParam = new \SoapParam($mac, "NewMACAddress");
